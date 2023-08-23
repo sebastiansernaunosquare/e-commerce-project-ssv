e067a0fb5837c670de00cf7c1fd85a426700615a
sebastian.serna <sebastian.serna@unosquare.com>
2023-08-23 16:57:02 -0500

    Feature: Database pool and user endpoints

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var swaggerUI = require("swagger-ui-express");
+var specs = require("./definitions");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -20,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
---
+++ backend/backend/definitions.js
@@ -0,0 +1,30 @@
+// const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
+    },
+
+    servers: [
+      {
+        url: "http://localhost:4001",
+        description: "My API Documentation",
+      },
+    ],
+  },
+  apis: ["./Routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+module.exports = specs;

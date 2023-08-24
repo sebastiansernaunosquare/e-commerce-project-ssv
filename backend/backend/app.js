4f39abf097bd0c5353931682b319fe12347816a2
sebastian.serna <sebastian.serna@unosquare.com>
2023-08-24 11:02:01 -0500

    Feature: Create routes for the products and swagger docs

--- backend/backend/app.js
+++ backend/backend/app.js
@@ -8,6 +8,7 @@ var specs = require("./definitions");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var productsRouter = require("./routes/products");
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
